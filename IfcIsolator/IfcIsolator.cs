6b3fefd0cf632540348d205de0c7ecc980936aa4
matheushsaba <matheushs15@hotmail.com>
2025-03-21 19:19:11 -0300

    Add isolator project

---
+++ IfcIsolator/IfcIsolator.cs
@@ -0,0 +1,102 @@
+﻿using Xbim.Common;
+using Xbim.Ifc;
+using Xbim.Ifc4.Interfaces;
+using Xbim.IO;
+
+namespace IfcIsolator
+{
+    public static class IfcIsolator
+    {
+        const string IFC_FILE_EXTENSION = ".ifc";
+        const string OUTPUT_FILE_SUFFIX = "_Isolated";
+
+        public static void SplitByEntityLabels(string sourceFilePath, string outputFolderPath, int[] entityLabels)
+        {
+            using (var sourceModel = IfcStore.Open(sourceFilePath))
+            {
+                var products = GetProductsByEntityLabel(sourceModel, entityLabels);
+                var fileName = Path.GetFileNameWithoutExtension(sourceFilePath);
+                var outputFileName = fileName + OUTPUT_FILE_SUFFIX + IFC_FILE_EXTENSION;
+                var outputFilePath = Path.Combine(outputFolderPath, outputFileName);
+
+                var xbimEditorCredentials = new XbimEditorCredentials()
+                {
+                    EditorsFamilyName = Environment.UserName,
+                    EditorsGivenName = Environment.UserName,
+                    EditorsOrganisationName = Environment.UserName,
+                    ApplicationFullName = "Ifc Isolator",
+                    ApplicationVersion = "1.0.0",
+                    ApplicationIdentifier = "Ifc Isolator",
+                    ApplicationDevelopersName = "Matheus Sabadin",
+                };
+
+                using (var targetModel = IfcStore.Create(xbimEditorCredentials, ((IModel)sourceModel).SchemaVersion, XbimStoreType.EsentDatabase))
+                {
+                    using (sourceModel.BeginEntityCaching())
+                    using (sourceModel.BeginInverseCaching())
+                    using (var txn = targetModel.BeginTransaction("InsertCopy with IfcProducts"))
+                    {
+                        var map = new XbimInstanceHandleMap(sourceModel, targetModel);
+                        targetModel.InsertCopy(products, true, false, map, true);
+                        txn.Commit();
+                    }
+                    targetModel.Header.FileDescription = sourceModel.Header.FileDescription;
+                    targetModel.Header.FileName = sourceModel.Header.FileName;
+                    targetModel.Header.FileSchema = sourceModel.Header.FileSchema;
+                    targetModel.Header.FileName.OriginatingSystem = sourceModel.Header.FileName.OriginatingSystem;
+                    targetModel.SaveAs(outputFilePath);
+                }
+            }
+        }
+
+        private static HashSet<IIfcProduct> GetProductsByEntityLabel(IModel model, int[] entityLabels)
+        {
+            var entityLabelsSet = new HashSet<int>(entityLabels);
+            var collectedProducts = new HashSet<IIfcProduct>();
+
+            var entityProducts = model
+                .Instances
+                .OfType<IIfcProduct>()
+                .Where(x => entityLabelsSet.Contains(x.EntityLabel));
+
+            foreach (var product in entityProducts)
+            {
+                collectedProducts.Add(product);
+                if (product is IIfcSpatialStructureElement)
+                {
+                    collectedProducts.UnionWith(GetProductHierarchyRecursively(product, collectedProducts));
+                }
+            }
+
+            return collectedProducts;
+        }
+
+        private static HashSet<IIfcProduct> GetProductHierarchyRecursively(IIfcObjectDefinition ifcObjectDefinition, HashSet<IIfcProduct> collectedProducts)
+        {
+            if (ifcObjectDefinition == null)
+            {
+                return collectedProducts;
+            }
+
+            // Only spatial elements can contain building elements
+            if (ifcObjectDefinition is IIfcSpatialStructureElement spatialElement)
+            {
+                // Use IfcRelContainedInSpatialElement to retrieve contained elements
+                var containedProducts = spatialElement.ContainsElements.SelectMany(rel => rel.RelatedElements);
+                foreach (var product in containedProducts)
+                {
+                    collectedProducts.Add(product);
+                }
+            }
+
+            // Use IfcRelAggregares to get the spatial decomposition of spatial structure elements
+            var childObjectDefinitions = ifcObjectDefinition.IsDecomposedBy.SelectMany(rel => rel.RelatedObjects);
+            foreach (var childDefinition in childObjectDefinitions)
+            {
+                GetProductHierarchyRecursively(childDefinition, collectedProducts);
+            }
+
+            return collectedProducts;
+        }
+    }
+}
--- IfcIsolator/Program.cs
+++ IfcIsolator/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Validate that we have at least three arguments
+            if (args.Length < 3)
+            {
+                Console.WriteLine("Usage: Program <inputPath> <outputPath> <entityLabels>");
+                Console.WriteLine("Example: Program \"C:\\input.ifc\" \"C:\\output.ifc\" \"10 20 30 100\"");
+                return;
+            }
+
+            // Read the arguments
+            string inputPath = args[0];
+            string outputPath = args[1];
+            string labelsRaw = args[2]; // e.g., "10 20 30"
+
+            // Split the third argument on spaces and parse them as integers
+            int[] entityLabels = labelsRaw
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            // Now call into your IfcSplitter logic
+            IfcIsolator.SplitByEntityLabels(inputPath, outputPath, entityLabels);
         }
     }
 }
