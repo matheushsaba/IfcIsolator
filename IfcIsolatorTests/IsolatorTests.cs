16a6df9f17d0dd3f0c1d22a3ab6fd09e4758549b
matheushsaba <matheushs15@hotmail.com>
2025-03-24 21:26:40 -0300

    Add new test

--- IfcIsolatorTests/IsolatorTests.cs
+++ IfcIsolatorTests/IsolatorTests.cs
@@ -5,7 +5,7 @@ namespace IfcIsolatorTests
     public class IsolatorTests
     {
         [Fact]
-        public void Test1()
+        public void Isolate_Wall()
         {
             // ConsoleRunner.exe "C:\\Users\\Matheus\\Desktop\\TestFiles\\Ifc4_Revit_STR.ifc" "C:\\Users\\Matheus\\Desktop\\TestFiles" 328
             var sourcePath = "C:\\Users\\Matheus\\Desktop\\TestFiles\\Ifc4_Revit_STR.ifc";
@@ -17,6 +17,21 @@ namespace IfcIsolatorTests
                 return;
             }
 
+            Isolator.SplitByEntityLabels(sourcePath, outputPath, entityLabels);
+        }
+
+        [Fact]
+        public void Isolate_Space()
+        {
+            // ConsoleRunner.exe "C:\\Users\\Matheus\\Desktop\\TestFiles\\Ifc4_Revit_STR.ifc" "C:\\Users\\Matheus\\Desktop\\TestFiles" 328
+            var sourcePath = "C:\\Users\\Matheus\\Desktop\\TestFiles\\Ifc4_Revit_STR.ifc";
+            var outputPath = "C:\\Users\\Matheus\\Desktop\\TestFiles";
+            var entityLabels = new int[] { 257 };
+
+            if (!Path.Exists(sourcePath))
+            {
+                return;
+            }
 
             Isolator.SplitByEntityLabels(sourcePath, outputPath, entityLabels);
         }
